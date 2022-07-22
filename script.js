5737d486eb3c44e11813372cd5f8bc93214f634d
prajwal khupat <prajwalkhupat5@gmail.com>
2022-07-22 21:27:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const cursor = document.querySelector('.cursor');
+
+window.addEventListener('mousemove',(e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+    cursor.setAttribute('data-fromTop',(cursor.offsetTop - scrollY))
+    //console.log(e.pageX,e.pageY);
+});
+window.addEventListener('scroll', () =>{
+    const fromTop = parseInt(cursor.getAttribute('data-fromTop'));
+    cursor.style.top = scrollY + fromTop + 'px';
+    console.log(fromTop)
+});
+ window.addEventListener('click', ()=>{
+    if(cursor.classList.contains('click')){
+        cursor.classList.remove('click');
+        // Triggering a Dom Reflow
+        void cursor.offsetWidth;
+        cursor.classList.add('click');
+    }
+    else{
+        cursor.classList.add('click')
+    }
+ });
